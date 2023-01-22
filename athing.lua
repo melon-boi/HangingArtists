3a21399dbff2655212580ea6b8fba38e0c23405d
melon boi <80632439+melon-boi@users.noreply.github.com>
2023-01-22 12:30:28 +0000

    Update athing.lua

--- athing.lua
+++ athing.lua
@@ -1,3 +1,5 @@
+local SolarisLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/sol"))()
+
 local win = SolarisLib:New({
   Name = "Hanging Artists v1.1",
   FolderToSave = "SolarisLibStuff"
