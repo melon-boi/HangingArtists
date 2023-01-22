486b22e061efd2f41a05d667546981b86071ef23
melon boi <80632439+melon-boi@users.noreply.github.com>
2023-01-22 13:33:10 +0000

    Update Credits.lua

--- Credits.lua
+++ Credits.lua
@@ -1,6 +1,5 @@
 local SolarisLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/sol"))()
 --tab:Section(title <string>)
-local sec2 = tab2:Section("Credits")
 sec2:Label("Owners - melon boi#2511, MrAWAY#0069")
 sec2:Label("Developers - melonboi#2511")
 sec2:Label("Discord Server - https://discord.gg/rtcz9nUP57")
