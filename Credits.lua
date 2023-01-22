931d714247cd12055ca9ac38a0cc1a9082b62a8f
melon boi <80632439+melon-boi@users.noreply.github.com>
2023-01-22 13:12:03 +0000

    Update Credits.lua

--- Credits.lua
+++ Credits.lua
@@ -4,7 +4,6 @@ local SolarisLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bl
 local tab2 = win:Tab("Credits")
 
 --tab:Section(title <string>)
-local sec = tab:Section("Main")
 local sec2 = tab2:Section("Credits")
 sec2:Label("Owners - melon boi#2511, MrAWAY#0069")
 sec2:Label("Developers - melonboi#2511")
