adc6252dd14d444c774cbbe66f197aee09b58a31
melon boi <80632439+melon-boi@users.noreply.github.com>
2023-01-22 13:34:11 +0000

    Update Anti-Skid.lua

--- Anti-Skid.lua
+++ Anti-Skid.lua
@@ -1,6 +1,6 @@
 local SolarisLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/sol"))()
 
 local win = SolarisLib:New({
-  Name = "Hanging Artists",
+  Name = "Hanging Artists v1.1",
   FolderToSave = "SolarisLibStuff"
 })
