b0550615c6fd124267ab80a169a061bded8f493d
melon boi <80632439+melon-boi@users.noreply.github.com>
2023-01-22 12:44:31 +0000

    Update athing.lua

--- athing.lua
+++ athing.lua
@@ -9,13 +9,6 @@ local win = SolarisLib:New({
 local tab = win:Tab("Main Script")
 local tab2 = win:Tab("Credits")
 
---tab:Section(title <string>)
-local sec = tab:Section("Main")
-local slider = sec:Slider("Delay", 0,2,0,0.1,"Delay", function(t)
-  print(t)
-end)
-
-
 local sec2 = tab2:Section("Credits")
 sec2:Label("Owners - melon boi#2511, MrAWAY#0069")
 sec2:Label("Developers - melonboi#2511")
