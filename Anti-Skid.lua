ca91afff0c02266405245069a822e000f03eccff
melon boi <80632439+melon-boi@users.noreply.github.com>
2023-01-25 15:43:17 +0000

    Update Anti-Skid.lua

--- Anti-Skid.lua
+++ Anti-Skid.lua
@@ -1,2 +1,3 @@
 local lib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")() 
+_G.closeBind = Enum.KeyCode.F2
 local win = lib:Window("Hanging Artist", "Best script for SA!", Color3.fromRGB(226, 42, 232), _G.closeBind) 
